d52625cbe147d8790764e80d96938c9a07c56c55
prog <prog@progs-MacBook-Pro.local>
2019-01-07 00:48:48 +0500

    setting up podcast

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by prog on 1/7/19.
+//  Copyright © 2019 prog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
